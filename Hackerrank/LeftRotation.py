1a809a56da9efbb784085bca5310e7d99065a580
Soojung Kim <julieline@naver.com>
2018-06-26 00:10:22 +0900

    left rotation

--- Hackerrank/LeftRotation.py
+++ Hackerrank/LeftRotation.py
@@ -1,13 +1,19 @@
-n, d = map(int, input().split())
+## input : 5 4 / 1 2 3 4 5
+## output : 5 4 3 2 1 (str)
 
-arr = []
-for i in range(1, n+1):
-    arr.append(i)
+n, d = map(int, input().split())
+arr = list(map(int, input().rstrip().split()))
+
+# arr = []
+# for i in range(1, n+1):
+#     arr.append(i)
 print(arr)
 
 for i in range(1, d+1):
     first = arr[0]
     arr.remove(arr[0])
-    arr.append(arr[0])
+    arr.append(first)
 
-print(arr)
+sep = " "
+result = sep.join(map(str, arr))
+print(result)
