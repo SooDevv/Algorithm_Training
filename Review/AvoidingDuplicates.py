299bc4131e9a8a9d90bbb77cd79001d47444bde1
kim soojung <julieline@naver.com>
2018-07-06 23:26:30 +0900

    python training

--- Review/AvoidingDuplicates.py
+++ Review/AvoidingDuplicates.py
@@ -1,12 +1,26 @@
 # 멤버쉽 연산자 사용해 볼 것
 # in, not in
 
-s = input()
+# s = input()
+#
+# check = []
+# while True:
+#     if s in check:
+#         break
+#     print(s)
+#     check.append(s)
+#     s = input()
 
-check = []
+# sol2
+word_list = []
 while True:
-    if s in check:
+    word = input()
+    if word is '':
         break
-    print(s)
-    check.append(s)
-    s = input()
+    try:
+        word_list.index(word) ##word 가 wordlist 에 없다면 ValueError 가 나서 exccept 문 호출
+    except:
+        word_list.append(word)
+
+for i in word_list:
+    print(i)
