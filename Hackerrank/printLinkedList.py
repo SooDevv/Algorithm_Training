82b2367a4b1907880154f3bbe4ef670b1f5c302f
kim soojung <julieline@naver.com>
2018-07-16 09:50:08 +0900

    update main method of printLinkedList

--- Hackerrank/printLinkedList.py
+++ Hackerrank/printLinkedList.py
@@ -24,3 +24,11 @@ def printLinkedList(head):
             print(cur.data)
             cur = cur.next
 
+llist_count = int(input())
+llist = SinglyLinkedList()
+
+for _ in range(llist_count):
+    llist_item = int(input())
+    llist.insert_node(llist_item)
+
+printLinkedList(llist.head)
