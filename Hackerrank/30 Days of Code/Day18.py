abb04d0260c4795ff0f5188a1f4cdf48013afe10
kim soojung <julieline@naver.com>
2018-10-04 17:36:23 +0900

    #10.04 algorithm-training

---
+++ Hackerrank/30 Days of Code/Day18.py
@@ -0,0 +1,42 @@
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+
+    def pushCharacter(self, s):
+        self.stack.append(s)
+        return self.stack
+
+    def enqueueCharacter(self, s):
+        self.queue.append(s)
+        return self.stack
+
+    def popCharacter(self):
+        return self.stack.pop()
+
+    def dequeueCharacter(self):
+        return self.queue.pop(0)
+
+
+if __name__ == '__main__':
+    s = input()
+
+    obj = Solution()
+
+    l = len(s)
+
+    for i in range(l):
+        obj.pushCharacter(s[i])
+        obj.enqueueCharacter(s[i])
+
+    isPalindrome=True
+
+    for i in range(l//2):
+        if obj.popCharacter()!=obj.dequeueCharacter():
+            isPalindrome=False
+            break
+
+    if isPalindrome:
+        print("The word, "+s+", is a palindrome.")
+    else:
+        print("The word, "+s+", is not a palindrome.")
